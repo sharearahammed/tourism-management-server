22540ca9c9f64c85b5318a1a1cc6d7cd3e58be7e
Sharear Ahammed <sharearahammed@gmail.com>
2024-04-26 22:14:28 +0600

    touristsSpotDB added

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.netgysa.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri);
@@ -19,16 +18,90 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
 
+    const database = client.db("touristsSpotDB");
+    const touristsSpotCollection = database.collection("touristsSpot");
+    const myTouristsSpotCollection = database.collection("myTouristsSpot");
+
+    app.get("/touristsSpot", async (req, res) => {
+      const cursor = touristsSpotCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/touristsSpot/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristsSpotCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/singleTouristsSpot/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = myTouristsSpotCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/alltouristsSpot/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log("email: ",email);
+      const query = { email: email };
+      const cursor = myTouristsSpotCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/alltouristsSpot", async (req, res) => {
+      const newtouristsSpot = req.body;
+      console.log(newtouristsSpot);
+      const result = await myTouristsSpotCollection.insertOne(newtouristsSpot);
+      res.send(result);
+    });
+
+    app.put("/alltouristsSpot/:email", async (req, res) => {
+      const updateSpot = req.body;
+      const email = req.params.email;
+      const filter = { email: email };
+      const spot = {
+        $set: {
+          touristsSpotName: updateSpot.touristsSpotName,
+          countryName: updateSpot.countryName,
+          location: updateSpot.location,
+          shortDescription: updateSpot.shortDescription,
+          averageCost: updateSpot.averageCost,
+          seasonality: updateSpot.seasonality,
+          travelTime: updateSpot.travelTime,
+          totaVisitorsPerYear: updateSpot.totaVisitorsPerYear,
+          photo: updateSpot.photo,
+        },
+      };
+      const result = await myTouristsSpotCollection.updateOne(
+        filter,
+        spot
+      );
+    });
+
+    app.delete("/alltouristsSpot/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const query = { email: email };
+      const result = await myTouristsSpotCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -36,12 +109,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
