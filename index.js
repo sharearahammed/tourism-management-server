e7ff31994f74489924d0a28dad1dd31543e05a29
Sharear Ahammed <sharearahammed@gmail.com>
2024-04-27 18:23:13 +0600

    add another get method by id

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ async function run() {
       const id = req.params.id;
       const query = {_id : new ObjectId(id)};
       const result = await touristsSpotCollection.findOne(query);
-      res.send(result); 
-    });
+      res.send(result);
+    }); 
 
     app.get("/alltouristsSpot", async (req, res) => {
       const cursor = myTouristsSpotCollection.find();
